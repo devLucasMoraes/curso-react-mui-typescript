2a58be2da7073fd15fe3e5810cc10c3131f30603
devLucasMoraes <devlucasmoraes@gmail.com>
2023-02-26 18:11:11 -0300

    Feat: configurado variáveis ambiente

--- src/shared/components/ferramentas-da-listagem/FerramentasDaListagem.tsx
+++ src/shared/components/ferramentas-da-listagem/FerramentasDaListagem.tsx
@@ -1,4 +1,5 @@
 import { Box, Button, Icon, Paper, TextField, useTheme } from '@mui/material';
+import { Environment } from '../../environment';
 
 
 interface IFerramentasDaListagemProps {
@@ -35,7 +36,7 @@ export const FerramentasDaListagem: React.FC<IFerramentasDaListagemProps> = ({
             {mostrarInputBusca && (
                 <TextField
                     size='small'
-                    placeholder='Pesquisar...'
+                    placeholder={Environment.INPUT_DE_BUSCA}
                     value={textoDaBusca}
                     onChange={(e) => aoMudarTextDeBusca?.(e.target.value)}
                 />
