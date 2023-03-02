4ae7e35ef4fb02e7413ed73e0323799354461f3f
devLucasMoraes <devlucasmoraes@gmail.com>
2023-03-01 21:15:25 -0300

    Feat: Adicionado tabela para listagem de pessoas

--- src/pages/pessoas/ListagemDePessoas.tsx
+++ src/pages/pessoas/ListagemDePessoas.tsx
@@ -1,31 +1,43 @@
-import { useEffect, useMemo } from 'react';
+import { Paper, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from '@mui/material';
+import { useEffect, useMemo, useState } from 'react';
 import { useSearchParams } from 'react-router-dom';
 import { FerramentasDaListagem } from '../../shared/components';
 import { useDebouce } from '../../shared/hooks';
 import { LayoutBaseDePagina } from '../../shared/layouts';
-import { PessoasService } from '../../shared/services/api/pessoas/PessoasService';
+import { IListagemPessoa, PessoasService } from '../../shared/services/api/pessoas/PessoasService';
 
 
 export const ListagemDePessoas: React.FC = () => {
 
     const [searchParams, setSearchParams] = useSearchParams();
 
-    const { debouce } = useDebouce(3000);
+    const { debouce } = useDebouce(500);
+
+    const [rows, setRows] = useState<IListagemPessoa[]>([]);
+    const [totalCount, setTotalCount] = useState(0);
+    const [isLoading, setIsLoading] = useState(true);
 
     const busca = useMemo(() => {
         return searchParams.get('busca') || '';
     }, [searchParams]);
 
     useEffect(() => {
+        setIsLoading(true);
 
         debouce(() => {
 
             PessoasService.getAll(1, busca)
                 .then((result) => {
+                    setIsLoading(false);
+
+
                     if (result instanceof Error) {
                         alert(result.message);
                     } else {
                         console.log(result);
+
+                        setTotalCount(result.totalCount);
+                        setRows(result.data);
                     }
                 });
 
@@ -44,6 +56,27 @@ export const ListagemDePessoas: React.FC = () => {
                     aoMudarTextDeBusca={texto => setSearchParams({ busca: texto }, { replace: true })}
                 />
             }>
+            <TableContainer component={Paper} variant="outlined" sx={{ m: 1, width: 'auto' }}>
+                <Table>
+                    <TableHead>
+                        <TableRow>
+                            <TableCell>Ações</TableCell>
+                            <TableCell>Nome completo</TableCell>
+                            <TableCell>Email</TableCell>
+                        </TableRow>
+                    </TableHead>
+
+                    <TableBody>
+                        {rows.map(row => (
+                            <TableRow key={row.id}>
+                                <TableCell>Ações</TableCell>
+                                <TableCell>{row.nomeCompleto}</TableCell>
+                                <TableCell>{row.email}</TableCell>
+                            </TableRow>
+                        ))}
+                    </TableBody>
+                </Table>
+            </TableContainer>
 
         </LayoutBaseDePagina>
     );
